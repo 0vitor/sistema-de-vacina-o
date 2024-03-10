db5ef77e8381043f26056aee18fe5f45ca17e0de
0vitor <w.wutaes@gmail.com>
2024-03-10 16:33:01 -0300

    test: create a candidate

--- src/tests/presentation/controllers/cadidate-controller.spec.ts
+++ src/tests/presentation/controllers/cadidate-controller.spec.ts
@@ -9,10 +9,12 @@ describe('Candidate controller', () => {
   const candidate = new Candidate();
   candidate.id = expect.any(String);
   candidate.cpf = '000.000.000-34';
-  candidate.ultima_data_covid = new Date();
   candidate.primeiro_nome = 'vitor';
   candidate.segundo_nome = 'lima';
   candidate.data_nascimento = expect.any(Date);
+  candidate.ultima_data_covid = expect.any(Date);
+  candidate.created_at = expect.any(Date);
+  candidate.updated_at = expect.any(Date);
 
   beforeAll(async () => {
     await prisma.candidato.deleteMany();
@@ -39,5 +41,25 @@ describe('Candidate controller', () => {
       console.log(err);
       expect(err.statusCode).toEqual(400);
     }
+  });
+
+  it('should create candidato', async () => {
+    const zodValidator = new ZodValidatorAdapter<CreateCandidateDto>(
+      candidateSchema,
+    );
+    const candidateController = new CandidateController(zodValidator);
+    const createCandidateDto = new CreateCandidateDto();
+
+    createCandidateDto.cpf = '000.000.000-34';
+    createCandidateDto.primeiro_nome = 'vitor';
+    createCandidateDto.segundo_nome = 'lima';
+    createCandidateDto.senha = 'odijf#$5sD2';
+    createCandidateDto.confirmacao_senha = 'odijf#$5sD2';
+    createCandidateDto.data_nascimento = new Date();
+    createCandidateDto.ultima_data_covid = new Date();
+
+    const createdCandidato =
+      await candidateController.create(createCandidateDto);
+    expect(createdCandidato).toEqual(candidate);
   });
 });
