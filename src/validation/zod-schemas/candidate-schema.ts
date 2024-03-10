8fca6f00bdf1f3486f78cfcee8f4fb5285cfbc1e
0vitor <w.wutaes@gmail.com>
2024-03-10 16:36:27 -0300

    refactor: add line break between fields

--- src/validation/zod-schemas/candidate-schema.ts
+++ src/validation/zod-schemas/candidate-schema.ts
@@ -4,20 +4,26 @@ export const candidateSchema = z
   .object({
     body: z.object({
       primeiro_nome: z.string({ required_error: 'Formato inválido' }).min(1),
+
       segundo_nome: z.string({ required_error: 'Formato inválido' }).min(1),
+
       cpf: z.string().refine((value) => {
         return /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value);
       }, 'Formato inválido'),
+
       data_nascimento: z.date({ required_error: 'Formato inválida' }),
+
       senha: z
         .string()
         .min(8, 'Deve conter no mínimo 8 caracteres')
         .refine((value) => {
           return /[A-Z]/.test(value) && /\d/.test(value);
         }, 'Deve conter pelo menos uma letra maiúscula e um número'),
+
       ultima_data_covid: z.date({
         required_error: 'Formato',
       }),
+
       confirmacao_senha: z
         .string()
         .min(8, 'Deve conter no mínimo 8 caracteres'),
