28a61ad68516b3ba878a1f49a2bd88f019df5891
0vitor <w.wutaes@gmail.com>
2024-03-14 12:32:10 -0300

    test: create makeSut and mockRequest helpers

--- src/tests/presentation/controllers/cadidato-controller.spec.ts
+++ src/tests/presentation/controllers/cadidato-controller.spec.ts
@@ -1,9 +1,40 @@
 import { Candidato } from '@src/domain/models';
 import { prisma } from '@src/infra/db/prisma/prisma-client';
-import { ZodValidatorAdapter } from '@src/presentation/adapters/zod-validator-adapter';
+import { ValidationResponse } from '@src/presentation/adapters/zod-validator-adapter';
 import { CandidatoController } from '@src/presentation/controllers/candidato-controller';
 import { CreateCandidatoDto } from '@src/presentation/dtos/create-candidato.dto';
-import { candidatoSchema } from '@src/validation/zod-schemas';
+import { badRequestError } from '@src/presentation/helpers/http-helper';
+import { Validator } from '@src/presentation/protocols/validation';
+
+const mockRequest = (): CreateCandidatoDto => {
+  const createCandidatoDto = new CreateCandidatoDto();
+  createCandidatoDto.cpf = '000.000.000-34';
+  createCandidatoDto.primeiro_nome = 'vitor';
+  createCandidatoDto.segundo_nome = 'lima';
+  createCandidatoDto.senha = 'odijf#$5sD2';
+  createCandidatoDto.confirmacao_senha = 'odijf#$5sD2';
+  createCandidatoDto.data_nascimento = new Date();
+  createCandidatoDto.ultima_data_covid = new Date();
+
+  return createCandidatoDto;
+};
+
+const makeSut = () => {
+  class ValidatorStub implements Validator<CreateCandidatoDto> {
+    body: CreateCandidatoDto;
+
+    result: ValidationResponse = { isValid: true, message: [] };
+
+    validate(body: CreateCandidatoDto): ValidationResponse {
+      this.body = body;
+      return this.result;
+    }
+  }
+
+  const validator = new ValidatorStub();
+  const sut = new CandidatoController(validator);
+  return { sut, validator };
+};
 
 describe('Candidato controller', () => {
   const candidato = new Candidato();
@@ -16,37 +47,26 @@ describe('Candidato controller', () => {
   candidato.created_at = expect.any(Date);
   candidato.updated_at = expect.any(Date);
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     await prisma.candidato.deleteMany();
   });
 
   it('should return 400 if validation throw', async () => {
-    const zodValidator = new ZodValidatorAdapter<CreateCandidatoDto>(
-      candidatoSchema,
+    const { sut, validator } = makeSut();
+    const error: ValidationResponse = { isValid: false, message: ['error'] };
+    validator.result = error;
+    expect(sut.create(mockRequest())).rejects.toEqual(
+      badRequestError(error.message),
     );
-    const candidatoController = new CandidatoController(zodValidator);
-    const createCandidatoDto = new CreateCandidatoDto();
-
-    createCandidatoDto.cpf = '00.000.000-34';
-    createCandidatoDto.primeiro_nome = 'vitor';
-    createCandidatoDto.segundo_nome = 'lima';
-    createCandidatoDto.senha = 'odijf#$5sD2';
-    createCandidatoDto.confirmacao_senha = 'odijf#$5sD2';
-    createCandidatoDto.data_nascimento = new Date();
-    createCandidatoDto.ultima_data_covid = new Date();
-
-    try {
-      await candidatoController.create(createCandidatoDto);
-    } catch (err: any) {
-      expect(err.statusCode).toEqual(400);
-    }
   });
 
-  it('should create candidato', async () => {
-    const zodValidator = new ZodValidatorAdapter<CreateCandidatoDto>(
-      candidatoSchema,
-    );
-    const candidatoController = new CandidatoController(zodValidator);
+  it('Should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut();
+    const createdCandidato = await sut.create(mockRequest());
+    expect(createdCandidato).toEqual(candidato);
+  });
+
+  /* it('should check if candidate has more than 18 years old', async () => {
     const createCandidatoDto = new CreateCandidatoDto();
 
     createCandidatoDto.cpf = '000.000.000-34';
@@ -54,11 +74,16 @@ describe('Candidato controller', () => {
     createCandidatoDto.segundo_nome = 'lima';
     createCandidatoDto.senha = 'odijf#$5sD2';
     createCandidatoDto.confirmacao_senha = 'odijf#$5sD2';
-    createCandidatoDto.data_nascimento = new Date();
+    createCandidatoDto.data_nascimento = new Date('2002-03-12T20:08:24.741Z');
     createCandidatoDto.ultima_data_covid = new Date();
 
-    const createdCandidato =
+    const createdCandidato: Candidato =
       await candidatoController.create(createCandidatoDto);
-    expect(createdCandidato).toEqual(candidato);
-  });
+
+    const result = await candidatoController.checkResearchEligibility(
+      createdCandidato.id,
+    );
+
+    console.log(result);
+  }); */
 });
