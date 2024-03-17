b17381253b28158c5e0c99247d5ebd00332619e4
0vitor <w.wutaes@gmail.com>
2024-03-16 22:33:40 -0300

    test: ensure DbAddAccount calls addAccountRepository with correct values

--- src/tests/data/usecases/add-account.spec.ts
+++ src/tests/data/usecases/add-account.spec.ts
@@ -1,38 +1,62 @@
+import { AddAccountRepository } from '@src/data/protocols/add-account-respository';
 import { Hasher } from '@src/data/protocols/hasher';
 import { DbAddAccount } from '@src/data/usecases/db-add-account';
+import { Candidato } from '@src/domain/models';
 import { CreateCandidatoDto } from '@src/presentation/dtos/create-candidato.dto';
+import { throwError } from '@src/tests/throw-error';
 
 interface sutTypes {
   sut: DbAddAccount;
   hasherStub: Hasher;
+  addAccountRepository: AddAccountRepository;
 }
 
-const makeInput = (): CreateCandidatoDto => {
+const makeUser = (): CreateCandidatoDto => {
   const dto = new CreateCandidatoDto();
-  dto.cpf = '000.000.000-34';
+  dto.cpf = '000.000.000-00';
   dto.primeiro_nome = 'vitor';
   dto.segundo_nome = 'lima';
   dto.senha = 'valid_password';
-  dto.confirmacao_senha = 'odijf#$5sD2';
+  dto.confirmacao_senha = 'valid_password';
   dto.data_nascimento = new Date();
   dto.ultima_data_covid = new Date();
   return dto;
 };
 
-const makeSut = (): sutTypes => {
+const makeHasher = () => {
   class HasherStub implements Hasher {
-    input: string;
+    user: string;
 
     async hash(plaintext: string): Promise<string> {
-      this.input = plaintext;
+      this.user = plaintext;
       return 'hashed_password';
     }
   }
+  return new HasherStub();
+};
 
-  const hasherStub = new HasherStub();
-  const sut = new DbAddAccount(hasherStub);
+const makeAddAccountRepository = () => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add(accountData: CreateCandidatoDto): Promise<Candidato> {
+      const createdAccount = new Candidato();
+      const result = Object.assign(createdAccount, accountData, {
+        id: 'valid_id',
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
 
-  return { sut, hasherStub };
+      return result;
+    }
+  }
+
+  return new AddAccountRepositoryStub();
+};
+
+const makeSut = (): sutTypes => {
+  const hasherStub = makeHasher();
+  const addAccountRepository = makeAddAccountRepository();
+  const sut = new DbAddAccount(hasherStub, addAccountRepository);
+  return { sut, hasherStub, addAccountRepository };
 };
 
 describe('AddAccount Usecase', () => {
@@ -40,8 +64,30 @@ describe('AddAccount Usecase', () => {
     const { sut, hasherStub } = makeSut();
     const hasherSpy = jest.spyOn(hasherStub, 'hash');
 
-    await sut.add(makeInput());
+    await sut.add(makeUser());
 
     expect(hasherSpy).toHaveBeenCalledWith('valid_password');
+  });
+
+  it('Should throw if Hasher throws', async () => {
+    const { sut, hasherStub } = makeSut();
+    jest.spyOn(hasherStub, 'hash').mockImplementationOnce(throwError);
+
+    expect(sut.add(makeUser())).rejects.toEqual(new Error());
+  });
+
+  it('Should call AddAccountRepository with correct values', async () => {
+    const { sut, addAccountRepository } = makeSut();
+    const addAccountRepositorySpy = jest.spyOn(addAccountRepository, 'add');
+
+    const user = makeUser();
+    await sut.add(user);
+
+    const userWithHashedPassword = makeUser();
+    userWithHashedPassword.senha = 'hashed_password';
+
+    expect(addAccountRepositorySpy).toHaveBeenCalledWith(
+      userWithHashedPassword,
+    );
   });
 });
